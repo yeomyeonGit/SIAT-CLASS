03be9154b24a8224be5d66b5a5901aab06d80244
cmion <yeomyeongforbuisness@gmail.com>
2025-05-23 10:40:24 +0900

    250523 docker

--- devops-app/backend/src/main/java/backend/backend/config/WebConfig.java
+++ devops-app/backend/src/main/java/backend/backend/config/WebConfig.java
@@ -1,19 +1,18 @@
-package com.siat.testspring.config;
-
+package backend.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
+
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry
-            .addMapping("/**")
+        registry.addMapping("/**")
+            .allowedHeaders("*")
+            
             .allowedOrigins("http://localhost:3000")
-            .allowedMethods("*");
+            .allowedMethods("*")
+            .allowCredentials(true) ;
     }
 }
